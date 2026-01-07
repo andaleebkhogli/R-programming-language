95c8a29867e69a74ba7897d4ebbe836f5bbbe4fd
Andaleeb Khogli <andaleebkhog@gmail.com>
2026-01-07 13:49:59 +0400

    Markdown

---
+++ Module4_Lesson03_Facet.R
@@ -0,0 +1,52 @@
+hotel_bookings <- read.csv("D:\\Studying\\R Language\\GDA_Practice\\hotel_bookings.csv")
+
+head(hotel_bookings)
+
+colnames(hotel_bookings)
+
+install.packages('ggplot2')
+library(ggplot2)
+
+ggplot(data = hotel_bookings) +
+  geom_bar(mapping = aes(x = distribution_channel))
+
+
+ggplot(data = hotel_bookings) +
+  geom_bar(mapping = aes(x = distribution_channel, fill=deposit_type))
+
+
+ggplot(data = hotel_bookings) +
+  geom_bar(mapping = aes(x = distribution_channel, fill=market_segment))
+
+
+
+ggplot(data = hotel_bookings) +
+  geom_bar(mapping = aes(x = distribution_channel)) +
+  facet_wrap(~deposit_type )
+
+
+ggplot(data = hotel_bookings) +
+  geom_bar(mapping = aes(x = distribution_channel)) +
+  facet_wrap(~deposit_type) +
+  theme(axis.text.x = element_text(angle = 45))
+
+
+
+ggplot(data = hotel_bookings) +
+  geom_bar(mapping = aes(x = distribution_channel)) +
+  facet_wrap(~market_segment) +
+  theme(axis.text.x = element_text(angle = 45))
+
+
+
+ggplot(data = hotel_bookings) +
+  geom_bar(mapping = aes(x = distribution_channel)) +
+  facet_grid(~deposit_type) +
+  theme(axis.text.x = element_text(angle = 45))
+
+
+
+ggplot(data = hotel_bookings) +
+  geom_bar(mapping = aes(x = distribution_channel)) +
+  facet_wrap(~deposit_type~market_segment) +
+  theme(axis.text.x = element_text(angle = 45))
---
+++ Module4_Lesson03_Filters.R
@@ -0,0 +1,42 @@
+hotel_bookings <- read.csv("D:\\Studying\\R Language\\GDA_Practice\\hotel_bookings.csv")
+
+head(hotel_bookings)
+
+colnames(hotel_bookings)
+
+install.packages('ggplot2')
+library(ggplot2)
+
+ggplot(data = hotel_bookings) +
+  geom_point(mapping = aes(x = lead_time, y = children))
+
+
+ggplot(data = hotel_bookings) +
+  geom_point(mapping = aes(x = lead_time, y = children))
+
+
+ggplot(data = hotel_bookings) +
+  geom_bar(mapping = aes(x = hotel, fill = market_segment))
+
+
+ggplot(data = hotel_bookings) +
+  geom_bar(mapping = aes(x = hotel)) +
+  facet_wrap(~market_segment)
+
+
+install.packages('tidyverse')
+library(tidyverse)
+
+
+
+
+onlineta_city_hotels <- filter(hotel_bookings, 
+                               (hotel=='City Hotel' & 
+                                  hotel_bookings$market_segment=='Online TA'))
+View(onlineta_city_hotels)
+
+onlineta_city_hotels_v2 <- hotel_bookings %>%
+  filter(hotel=="City Hotel") %>%
+  filter(market_segment=="Online TA")
+
+View(onlineta_city_hotels_v2)
